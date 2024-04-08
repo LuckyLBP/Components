7f766a0ec47b97f99a06f7b8d77fd07b73ea104a
Lucas <lucas.pohl@hotmail.com>
2024-04-08 09:26:18 +0200

    confetti

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import React from "react";
 import SearchBtn from "./Components/SearchBtn/SearchBtn";
 import "./App.scss";
 
 function App() {
+  const handleSearch = () => {
+    console.log("Search button clicked");
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      console.log("Enter pressed");
+      event.preventDefault();
+    }
+  };
+
   return (
     <>
-      <SearchBtn />
+      <SearchBtn
+        placeholder="Search..."
+        onSearch={handleSearch}
+        onKeyPress={handleKeyPress}
+      />
     </>
   );
 }
